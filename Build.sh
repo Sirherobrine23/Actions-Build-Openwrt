6d51a1f2f94f8cba309c37981eb1c472052e6fa0
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-10 19:13:03 -0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -31,12 +31,15 @@ cd /home/copiler/
 if [ $UID == '0' ] ;then
     echo 'root !WARNING!'
     export FORCE_UNSAFE_CONFIGURE=1
-else
-    SUDO='sudo'
 fi
 clone(){
-    git clone --depth 1 $URL -b $BRANCH /home/copiler/openwrt && mkdir /home/copiler/openwrt/bin && \
-    echo "14Gb free to bin files";$SUDO mount --bind /mnt /home/copiler/openwrt/bin || echo 'Erro in to mount --bind';exit 23
+    git clone --depth 1 $URL -b $BRANCH /home/copiler/openwrt && \
+    if ! ln -s /mnt /home/copiler/openwrt/bin ;then
+        echo "14Gb free to bin files" 
+    else
+        echo 'Erro in to mount --bind'
+        exit 23
+    fi
     df -hT . && df -tT /mnt
     status1=1
 }

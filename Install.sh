a45a98ff92260ad22a76bc714782cb5b7b7d7f41
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-10 16:01:28 -0300

    push

--- Install.sh
+++ Install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-sudo apt-get -qq update &> Apt-Log.txt
-sudo apt install -y curl &> Apt-Log.txt
-sudo apt -y install dos2unix git zip rsync mkisofs $(curl -fsSL https://raw.githubusercontent.com/P3TERX/openwrt-list/master/depends-ubuntu-2004) &> Apt-Log.txt
-sudo apt-get -qq autoremove --purge &> Apt-Log.txt
+[ $EUID != 0 ] && {
+    SUDO=sudo
+    echo -e "${INFO} You may need to enter a password to authorize."
+}
+$SUDO echo || exit 1
+$SUDO apt-get -qq update &> Apt-Log.txt
+$SUDO apt install -y curl &> Apt-Log.txt
+$SUDO apt -y install dos2unix git zip rsync mkisofs $(curl -fsSL https://raw.githubusercontent.com/P3TERX/openwrt-list/master/depends-ubuntu-2004) &> Apt-Log.txt
+$SUDO apt-get -qq autoremove --purge &> Apt-Log.txt
 # Remove Swap
-sudo swapoff -a
-sudo rm -rfv /mnt/swap*
-sudo chown $USER:$USER /mnt
-sudo chmod 777 /mnt
+$SUDO swapoff -a
+$SUDO rm -rfv /mnt/swap*
+$SUDO chown $USER:$USER /mnt
+$SUDO chmod 777 /mnt

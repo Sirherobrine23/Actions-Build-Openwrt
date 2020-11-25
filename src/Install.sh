2c0749ae69e9c90025e0a5c56ab223a7ce693889
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-11-25 17:23:51 -0300

    Update Install.sh

--- src/Install.sh
+++ src/Install.sh
@@ -1,95 +1,95 @@
 #!/bin/bash
 # 
-source /etc/os-release
-if [ $ID == "ubuntu" ];then
-    echo "Adicionando mais alguns repositorios"
-    echo "# Ubuntu bionic
-    deb http://archive.ubuntu.com/ubuntu/ focal main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted
-    deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted
-    deb http://archive.ubuntu.com/ubuntu/ focal universe
-    deb-src http://archive.ubuntu.com/ubuntu/ focal universe
-    deb http://archive.ubuntu.com/ubuntu/ focal-updates universe
-    deb-src http://archive.ubuntu.com/ubuntu/ focal-updates universe
-    deb http://archive.ubuntu.com/ubuntu/ focal multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ focal multiverse
-    deb http://archive.ubuntu.com/ubuntu/ focal-updates multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ focal-updates multiverse
-    deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
-    deb http://archive.canonical.com/ubuntu focal partner
-    deb-src http://archive.canonical.com/ubuntu focal partner
-    deb http://security.ubuntu.com/ubuntu/ focal-security main restricted
-    deb-src http://security.ubuntu.com/ubuntu/ focal-security main restricted
-    deb http://security.ubuntu.com/ubuntu/ focal-security universe
-    deb-src http://security.ubuntu.com/ubuntu/ focal-security universe
-    deb http://security.ubuntu.com/ubuntu/ focal-security multiverse
-    deb-src http://security.ubuntu.com/ubuntu/ focal-security multiverse
+# source /etc/os-release
+# if [ $ID == "ubuntu" ];then
+#     echo "Adicionando mais alguns repositorios"
+#     echo "# Ubuntu bionic
+#     deb http://archive.ubuntu.com/ubuntu/ focal main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ focal universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal universe
+#     deb http://archive.ubuntu.com/ubuntu/ focal-updates universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal-updates universe
+#     deb http://archive.ubuntu.com/ubuntu/ focal multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ focal-updates multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal-updates multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+#     deb http://archive.canonical.com/ubuntu focal partner
+#     deb-src http://archive.canonical.com/ubuntu focal partner
+#     deb http://security.ubuntu.com/ubuntu/ focal-security main restricted
+#     deb-src http://security.ubuntu.com/ubuntu/ focal-security main restricted
+#     deb http://security.ubuntu.com/ubuntu/ focal-security universe
+#     deb-src http://security.ubuntu.com/ubuntu/ focal-security universe
+#     deb http://security.ubuntu.com/ubuntu/ focal-security multiverse
+#     deb-src http://security.ubuntu.com/ubuntu/ focal-security multiverse
 
-    # Ubuntu Bionic
-    deb http://archive.ubuntu.com/ubuntu/ bionic main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted
-    deb http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted
-    deb http://archive.ubuntu.com/ubuntu/ bionic universe
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic universe
-    deb http://archive.ubuntu.com/ubuntu/ bionic-updates universe
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates universe
-    deb http://archive.ubuntu.com/ubuntu/ bionic multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic multiverse
-    deb http://archive.ubuntu.com/ubuntu/ bionic-updates multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates multiverse
-    deb http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse
-    deb http://archive.canonical.com/ubuntu bionic partner
-    deb-src http://archive.canonical.com/ubuntu bionic partner
-    deb http://security.ubuntu.com/ubuntu/ bionic-security main restricted
-    deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted
-    deb http://security.ubuntu.com/ubuntu/ bionic-security universe
-    deb-src http://security.ubuntu.com/ubuntu/ bionic-security universe
-    deb http://security.ubuntu.com/ubuntu/ bionic-security multiverse
-    deb-src http://security.ubuntu.com/ubuntu/ bionic-security multiverse
+#     # Ubuntu Bionic
+#     deb http://archive.ubuntu.com/ubuntu/ bionic main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ bionic universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic universe
+#     deb http://archive.ubuntu.com/ubuntu/ bionic-updates universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates universe
+#     deb http://archive.ubuntu.com/ubuntu/ bionic multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ bionic-updates multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic-updates multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse
+#     deb http://archive.canonical.com/ubuntu bionic partner
+#     deb-src http://archive.canonical.com/ubuntu bionic partner
+#     deb http://security.ubuntu.com/ubuntu/ bionic-security main restricted
+#     deb-src http://security.ubuntu.com/ubuntu/ bionic-security main restricted
+#     deb http://security.ubuntu.com/ubuntu/ bionic-security universe
+#     deb-src http://security.ubuntu.com/ubuntu/ bionic-security universe
+#     deb http://security.ubuntu.com/ubuntu/ bionic-security multiverse
+#     deb-src http://security.ubuntu.com/ubuntu/ bionic-security multiverse
 
-    # Ubuntu Xenial
-    deb http://archive.ubuntu.com/ubuntu/ xenial main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted
-    deb http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted
-    deb http://archive.ubuntu.com/ubuntu/ xenial universe
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial universe
-    deb http://archive.ubuntu.com/ubuntu/ xenial-updates universe
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates universe
-    deb http://archive.ubuntu.com/ubuntu/ xenial multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial multiverse
-    deb http://archive.ubuntu.com/ubuntu/ xenial-updates multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates multiverse
-    deb http://archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
-    deb-src http://archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
-    deb http://archive.canonical.com/ubuntu xenial partner
-    deb-src http://archive.canonical.com/ubuntu xenial partner
-    deb http://security.ubuntu.com/ubuntu/ xenial-security main restricted
-    deb-src http://security.ubuntu.com/ubuntu/ xenial-security main restricted
-    deb http://security.ubuntu.com/ubuntu/ xenial-security universe
-    deb-src http://security.ubuntu.com/ubuntu/ xenial-security universe
-    deb http://security.ubuntu.com/ubuntu/ xenial-security multiverse
-    deb-src http://security.ubuntu.com/ubuntu/ xenial-security multiverse" >> /tmp/apt_sources.txt
-else
-    echo "# Debian Buster
-    deb http://deb.debian.org/debian buster main non-free contrib
-    deb http://deb.debian.org/debian buster-updates main non-free contrib
-    deb http://security.debian.org/debian-security/ buster/updates main non-free contrib
-    deb http://ftp.debian.org/debian buster-backports main non-free contrib
+#     # Ubuntu Xenial
+#     deb http://archive.ubuntu.com/ubuntu/ xenial main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted
+#     deb http://archive.ubuntu.com/ubuntu/ xenial universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial universe
+#     deb http://archive.ubuntu.com/ubuntu/ xenial-updates universe
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates universe
+#     deb http://archive.ubuntu.com/ubuntu/ xenial multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ xenial-updates multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates multiverse
+#     deb http://archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+#     deb-src http://archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+#     deb http://archive.canonical.com/ubuntu xenial partner
+#     deb-src http://archive.canonical.com/ubuntu xenial partner
+#     deb http://security.ubuntu.com/ubuntu/ xenial-security main restricted
+#     deb-src http://security.ubuntu.com/ubuntu/ xenial-security main restricted
+#     deb http://security.ubuntu.com/ubuntu/ xenial-security universe
+#     deb-src http://security.ubuntu.com/ubuntu/ xenial-security universe
+#     deb http://security.ubuntu.com/ubuntu/ xenial-security multiverse
+#     deb-src http://security.ubuntu.com/ubuntu/ xenial-security multiverse" >> /tmp/apt_sources.txt
+# else
+#     echo "# Debian Buster
+#     deb http://deb.debian.org/debian buster main non-free contrib
+#     deb http://deb.debian.org/debian buster-updates main non-free contrib
+#     deb http://security.debian.org/debian-security/ buster/updates main non-free contrib
+#     deb http://ftp.debian.org/debian buster-backports main non-free contrib
 
-    # Debian oldstable
-    deb http://deb.debian.org/debian oldstable main non-free contrib
-    deb http://deb.debian.org/debian oldstable-updates main non-free contrib
-    deb http://security.debian.org/debian-security/ oldstable/updates main non-free contrib
-    deb http://ftp.debian.org/debian oldstable-backports main non-free contrib" >> /tmp/apt_sources.txt
-fi
-cat /etc/apt/sources.list >> /tmp/apt_sources.txt
-sleep 1s
-cat /tmp/apt_sources.txt | sudo tee /etc/apt/sources.list &> /dev/null
+#     # Debian oldstable
+#     deb http://deb.debian.org/debian oldstable main non-free contrib
+#     deb http://deb.debian.org/debian oldstable-updates main non-free contrib
+#     deb http://security.debian.org/debian-security/ oldstable/updates main non-free contrib
+#     deb http://ftp.debian.org/debian oldstable-backports main non-free contrib" >> /tmp/apt_sources.txt
+# fi
+# cat /etc/apt/sources.list >> /tmp/apt_sources.txt
+# sleep 1s
+# cat /tmp/apt_sources.txt | sudo tee /etc/apt/sources.list &> /dev/null
 
 # APT
 echo "Updating APT Repositories"
